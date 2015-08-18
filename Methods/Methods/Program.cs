5d07af701e41868205a58e7f4eba13637472a1b0
Z.Dimitrov <zlatin.dimitrov84@gmail.com>
2015-08-18 14:37:17 +0300

    Added methods solution

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main();
+        /*{
+            Console.WriteLine("Telerik Corp.");
+            Console.WriteLine("www.telerik.com");
+        }
+
+        static void PrintAcademyLogo()
+        {
+            Console.WriteLine("Telerik Academy");
+            Console.WriteLine("Part of:");
+            PrintLogo();
+        }
+
+    
+
+    
+        static void Main()
+        {
+            PrintAcademyLogo();
+        }*/
+        int a = 9;
+        int b = 9;
+
+        int result = 0;
+
+        
+
+            
+
+    }
+}
